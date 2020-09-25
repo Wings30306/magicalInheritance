40e019fe38883ce9d3adca6a8ed8f1627632542d
Joke Heyndels <jo_hannah@outlook.com>
2020-09-25 09:25:50 +0200

    Create Mage class that inherits from Pupil which has an additional Cast method

--- Mage.cs
+++ Mage.cs
@@ -1,7 +1,18 @@
 // Mage.cs
 using System;
+using System.Reflection;
 
 namespace MagicalInheritance
 {
+    class Mage : Pupil
+    {
+        public Mage(string name) : base(name)
+        {}
 
+        public Storm CastRainStorm()
+        {
+            Storm storm = new Storm(Title, false, "rain");
+            return storm;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,12 @@ namespace MagicalInheritance
             Pupil pupil = new Pupil("Mezil-Kree");
             Storm storm = pupil.CastWindStorm();
             Console.WriteLine(storm.Announce());
+
+            Mage mage = new Mage("Gul'dan");
+            Storm mageRainStorm = mage.CastRainStorm();
+            Console.WriteLine(mageRainStorm.Announce());
+            Storm mageWindStorm = mage.CastWindStorm();
+            Console.WriteLine(mageWindStorm.Announce());
         }
     }
 }
